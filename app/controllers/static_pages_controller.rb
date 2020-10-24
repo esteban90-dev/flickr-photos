87a05bc3c7c6e0c56737b1867438d72032206e13
Stephen Allard <stephencallard@gmail.com>
2020-10-24 08:53:46 -0500

    Initial commit after corrupted git

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,32 @@
+class StaticPagesController < ApplicationController
+  def index
+    if params[:id]
+      #get photos for specific user
+      @urls = get_public_photos(params[:id])
+    else
+      #get random public photos and their respective user_ids
+      @urls_and_users = get_random_photos_with_user(5)
+    end
+  end
+
+  private
+
+  def get_flickr
+    Flickr.cache = '/tmp/flickr-api.yml'
+    Flickr.new(ENV['flickr_api_key'], ENV['flickr_api_secret'])
+  end
+
+  def get_public_photos(id)
+    flickr = get_flickr
+    photos = flickr.people.getPublicPhotos( :user_id => id )
+    photos.map{ |photo| Flickr.url_b(photo) }
+  end
+
+  def get_random_photos_with_user(count)
+    flickr = get_flickr
+    photos = flickr.photos.getRecent(:per_page => count)
+    user_ids = photos.map{ |photo| photo.owner }
+    photos = photos.map{ |photo| Flickr.url_n(photo) }
+    photos.zip(user_ids).to_h
+  end
+end
